e7d8579f4dac278251c41ce988c93ea55ef648f5
yasiir <yasira640@gmail.com>
2020-10-31 14:37:34 +0500

    screens installed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Profile from './screens/Profile';
+import Settings from './screens/Settings';
+
+
+export default class App extends Component {
+  state = {
+    profile: true
+  }
+  render() {
+    const { profile } = this.state;
+    return (
+      <div>
+
+        {
+          profile ?
+            <Profile />
+            :
+            <Settings />
+        }
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/screens/Profile.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+export default class Profile extends Component {
+    state = {
+        User: {
+            name: 'yasir',
+            year: '4th',
+            status: 'startup'
+        }
+    }
+    render() {
+        const { User } = this.state;
+        return (
+            <div style={{ padding: 15 }}>
+                <h2>
+                    Profile
+                </h2>
+                <p>
+                    Name: {User.name.toUpperCase()}
+                </p>
+                <p>
+                    Year: {User.year.toUpperCase()}
+                </p>
+                <p>
+                    Status: {User.status.toUpperCase()}
+                </p>
+            </div>
+        )
+    }
+}
