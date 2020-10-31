8fedda54967804e3fe05860e92a3c14972c41edb
yasiir <yasira640@gmail.com>
2020-10-31 14:45:21 +0500

    initilizing store

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import React, { Component } from 'react';
 import Profile from './screens/Profile';
 import Settings from './screens/Settings';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer } from './reducers/reducer'
+
+const store = createStore(reducer)
 
 export default class App extends Component {
   state = {
@@ -11,15 +16,16 @@ export default class App extends Component {
   render() {
     const { profile } = this.state;
     return (
-      <div>
-
-        {
-          profile ?
-            <Profile />
-            :
-            <Settings />
-        }
-      </div>
+      <Provider store={store}>
+        <div>
+          {
+            profile ?
+              <Profile />
+              :
+              <Settings />
+          }
+        </div>
+      </Provider>
     )
   }
 }
---
+++ src/reducers/reducer.js
@@ -0,0 +1,8 @@
+const initState = {
+    date: [],
+    loading: true
+}
+export const reducer = (state = initState, action) => {
+    return state;
+}
+
